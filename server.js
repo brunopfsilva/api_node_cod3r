a8d1ce9455333f47179afca06281eb8bf499d8a0
Bruno pf Silva <bruno.pfsilva@hotmail.com>
2019-12-28 13:16:00 +0000

    second commit

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const port = 3300
 const express = require('express');
 const app = express();
 
+app.get('/produtos',(req,res,next)=>{
+    console.log('Md1')
+    next()
+})
 
 app.get('/produtos',(req,res,next)=> { //padrao middleware
     res.send({nome: 'Notebook',preco: 123.45}) //objecto convertido automatico para json
@@ -10,6 +14,6 @@ app.get('/produtos',(req,res,next)=> { //padrao middleware
 
 
 app.listen(port,()=>{
-    console.log('Executando')
+    console.log(`Executando porta ${port}`)
 })
 
